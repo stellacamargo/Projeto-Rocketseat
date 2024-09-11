b0fa9802b23659d1afafc16b64442b61662b85d3
Stella Camargo <stellacv2001@hotmail.com>
2024-09-11 17:24:52 +0200

    apresenta metas realizadas

--- index.js
+++ index.js
@@ -45,6 +45,22 @@ const listarMetas = async () => {
     console.log("Meta(s) marcadas como concluída(s)")
 }
 
+const metasRelizadas = async () => {
+    const realizadas = metas.filter((meta) => {
+        return meta.checked
+    })
+
+    if (realizadas.length == 0) {
+        console.log('Não existem metas realizadas! :(')
+        return
+    }
+
+    await select({
+        message: "Metas Realizadas",
+        choices: [...realizadas]
+    })
+}
+
 const start = async () => {
 
     while (true) {
@@ -61,6 +77,10 @@ const start = async () => {
                     value: "listar"
                 },
                 {
+                    name: "Metas realizadas",
+                    value: "realizadas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -74,6 +94,9 @@ const start = async () => {
                 break
             case "listar":
                 await listarMetas()
+                break
+            case "realizadas":
+                await metasRelizadas()
                 break
             case "sair":
                 console.log("Até a próxima!")
